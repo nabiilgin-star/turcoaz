352f3b8a18e8424a28c8af4d517ede34e45323e1
nabiilgin-star <nabi.ilgin@gmail.com>
2026-07-22 23:26:48 +0300

    WhatsApp numarası güncellendi

--- app/components/WhatsAppButton/WhatsAppButton.js
+++ app/components/WhatsAppButton/WhatsAppButton.js
@@ -38,7 +38,7 @@ export default function WhatsAppButton() {
 
   return (
     <a
-      href="https://wa.me/40765288783"
+      href="https://wa.me/40730630063"
       target="_blank"
       rel="noopener noreferrer"
       className={`whatsapp-float ${isVisible ? "visible" : "hidden"}`}
