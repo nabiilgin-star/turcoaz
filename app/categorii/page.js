3571109408bf948e0d39f96907fef4d9314feee9
nabiilgin-star <nabi.ilgin@gmail.com>
2026-09-12 21:21:28 +0300

    SEO meta basliklari guncellendi

--- app/categorii/page.js
+++ app/categorii/page.js
@@ -3,6 +3,11 @@ import Footer from "../components/Footer/Footer";
 import CategoriesSection from "../components/Categories/CategoriesSection";
 import { getNavbarData } from "@/app/lib/get-nav-data";
 
+export const metadata = {
+  title: "Profile Aluminiu, Balustrade Sticlă și Compozit Panel (Bond) | Turcoaz",
+  description: "Gama completă de profile din aluminiu, sisteme pentru balustrade din sticlă securizată și panouri compozite (bond) pentru fațade. Depozit Popești-Leordeni.",
+};
+
 export default async function CategoriesPage() {
   const { categories, products, announcement } = await getNavbarData();
 
@@ -13,7 +18,6 @@ export default async function CategoriesPage() {
         products={products}
         announcement={announcement}
       />
-      {}
       <div style={{ paddingTop: "0" }}>
         <CategoriesSection className="gray-bg" categories={categories} />
       </div>
